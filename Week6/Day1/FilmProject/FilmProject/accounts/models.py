18c7a675ed6462c929d7508e6422c57729ce1b3d
DmitryArtamonov <artamonov2009@gmail.com>
2023-06-05 17:56:36 +0300

    w6d2 custom uder added (daily challenge)

--- Week6/Day1/FilmProject/FilmProject/accounts/models.py
+++ Week6/Day1/FilmProject/FilmProject/accounts/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
+# just for excercise, customUser is not used in apps
+class CustomUser(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50)
+    love_cats = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'last_name'
+    REQUIRED_FIELDS = ['first_name','last_name','love_cats']
 
-
-# Create your models here.
