31cb0e5b7178df1785c3bb2a543418dbd0e34304
DmitryArtamonov <artamonov2009@gmail.com>
2023-06-11 16:33:03 +0300

    w7d1 daily challenge added

--- Week7/Day1/ex-xp/students/students_app/views.py
+++ Week7/Day1/ex-xp/students/students_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT)
-
+from datetime import datetime
 
 from .models import Student
 from .serializers import StudentSerializer
@@ -11,7 +11,14 @@ from .serializers import StudentSerializer
 class Student_list(APIView):
 
     def get(self, request, *args, **kwargs):
-        queryset = Student.objects.all()
+        date_joined_str = self.request.query_params.get('date_joined')
+
+        if date_joined_str:
+            date_joined_dt = datetime.strptime(date_joined_str, "%Y-%m-%d").date()
+            queryset = Student.objects.filter(date_joined__date=date_joined_dt)
+        else:
+            queryset = Student.objects.all()
+
         serializer = StudentSerializer(queryset, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
@@ -28,6 +35,7 @@ class Student_list(APIView):
 class Student_detail(APIView):
 
     def get(self, request, pk, *args, **kwargs):
+
         student = get_object_or_404(Student, pk=pk)
         serializer = StudentSerializer(student)
         return Response(serializer.data, status=HTTP_200_OK)
