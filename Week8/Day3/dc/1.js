96594c7f6f3a7a540ad94ef1dc432b73b0380380
DmitryArtamonov <artamonov2009@gmail.com>
2023-06-27 21:07:48 +0300

    w8d3 dc added

---
+++ Week8/Day3/dc/1.js
@@ -0,0 +1,47 @@
+const gameInfo = [
+    {
+      username: "john",
+      team: "red",
+      score: 5,
+      items: ["ball", "book", "pen"]
+    },
+    {
+      username: "becky",
+      team: "blue",
+      score: 10,
+      items: ["tape", "backpack", "pen"]
+    },
+    {
+      username: "susy",
+      team: "red",
+      score: 55,
+      items: ["ball", "eraser", "pen"]
+    },
+    {
+      username: "tyson",
+      team: "green",
+      score: 1,
+      items: ["book", "pen"]
+    },
+   ];
+
+// Create an array using forEach that contains all the usernames from the gameInfo array, add an exclamation point (ie. “!”) to the end of every username.
+// The new array should look like this :
+// const usernames = ["john!", "becky!", "susy!", "tyson!"]
+
+const usernames = [];
+gameInfo.forEach((element) => {
+    usernames.push(element.username + '!')
+})
+
+// 2. Create an array using forEach that contains the usernames of all players with a score bigger than 5.
+// The new array should look like this :
+
+// const winners = ["becky", "susy"]
+
+const winners = gameInfo.filter((elem) => elem.score > 5).map((elem) => elem.username)
+
+
+// 3. Find and display the total score of the users. (Hint: The total score is 71)
+
+const totalScore = gameInfo.reduce((acc, elem) => acc + elem.score, 0)
