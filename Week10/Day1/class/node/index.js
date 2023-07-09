7a02d72c5f1b4b04a7ea9f72a76813fdf7e593ce
DmitryArtamonov <artamonov2009@gmail.com>
2023-07-09 14:36:04 +0300

    w10d1 ex_xp added

---
+++ Week10/Day1/class/node/index.js
@@ -0,0 +1,10 @@
+module.exports.first = () => {
+  console.log("First Export");
+}
+
+module.exports.second = () => {
+  console.log("Second Export");
+}
+
+console.log(module);
+console.log(module.exports);
